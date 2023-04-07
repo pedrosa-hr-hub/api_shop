a7d349d76689e391b29d5cf25d9f6429a6ae1d80
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-04-06 22:48:17 -0300

    create user CRUD functions

--- src/repositorys/user.repository.js
+++ src/repositorys/user.repository.js
@@ -6,3 +6,38 @@ export const createUser = async (data) => {
     });
     return user;
 };
+
+export const updateUser = async (data) => {
+    const user = await prisma.user.update({
+        where: {
+            id: data.id,
+        },
+        data: {
+            data,
+        },
+    });
+    return user;
+};
+
+export const deleteUser = async (data) => {
+    const user = await prisma.user.delete({
+        where: {
+            id: data.id,
+        },
+    });
+    return user;
+};
+
+export const findAllUser = async (data) => {
+    const user = await prisma.user.findMany();
+    return user;
+};
+
+export const findbyIdUser = async (data) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: data.id,
+        },
+    });
+    return user;
+};
