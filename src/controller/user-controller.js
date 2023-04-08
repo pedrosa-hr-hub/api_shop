eda15a63dcb498f44b769755b24875c9bbf4c7fc
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-04-08 19:53:45 -0300

    create user update and find one

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -2,12 +2,14 @@ import {
     createUser,
     deleteUser,
     findAllUser,
+    findbyIdUser,
+    updateUser,
 } from '../repositorys/user.repository';
 
 export const create = async (req, res) => {
     try {
-        const user = await createUser(req.body);
-        res.status(201).json(user);
+        const reqdata = await createUser(req.body);
+        res.status(201).json(reqdata);
     } catch (e) {
         res.status(400).json(e);
     }
@@ -15,7 +17,12 @@ export const create = async (req, res) => {
 
 export const drop = async (req, res) => {
     try {
-        await deleteUser(req.body);
+        const reqdata = req.body.id;
+        if (!reqdata.id) {
+            res.status(400).json('no ID in request body');
+        } else {
+            await deleteUser(data);
+        }
         res.status(202).json('Deleted!');
     } catch (e) {
         res.status(400).json(e);
@@ -24,8 +31,36 @@ export const drop = async (req, res) => {
 
 export const getAll = async (req, res) => {
     try {
-        const data = await findAllUser();
-        res.status(200).json(data);
+        const dbdata = await findAllUser();
+        res.status(200).json(dbdata);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+};
+
+export const getById = async (req, res) => {
+    try {
+        const reqdata = await req.body;
+        if (!reqdata.id) {
+            res.status(400).json('no ID in request body');
+        } else {
+            const dbdata = await findbyIdUser(reqdata);
+            res.status(200).json(dbdata);
+        }
+    } catch (e) {
+        res.status(400).json(e);
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const reqdata = await req.body;
+        if (!reqdata.id) {
+            res.status(400).json('no ID in request body');
+        } else {
+            const dbdata = await updateUser(reqdata);
+            res.status(202).json(dbdata);
+        }
     } catch (e) {
         res.status(400).json(e);
     }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,17 @@
-import { create, drop, getAll } from '../controller/user-controller';
+import {
+    create,
+    drop,
+    getAll,
+    getById,
+    update,
+} from '../controller/user-controller';
 
 const userRoutes = (app) => {
     app.post('/user', create);
     app.delete('/user', drop);
     app.get('/user', getAll);
+    app.get('/userid', getById);
+    app.put('/user', update);
 };
 
 export default userRoutes;
