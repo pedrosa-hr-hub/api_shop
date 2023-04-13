de764a338502db49e51298848e69515186f896fd
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-04-13 18:36:32 -0300

    ajust in json return

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -11,11 +11,9 @@ const bcrypt = require('bcrypt');
 export const create = async (req, res) => {
     try {
         const password = await req.body.password;
-
+        const reqdata = await req.body;
         var salt = bcrypt.genSaltSync(8);
         var hash = bcrypt.hashSync(password, salt);
-
-        const reqdata = await req.body;
 
         reqdata.password = hash;
 
@@ -31,7 +29,7 @@ export const drop = async (req, res) => {
     try {
         const reqdata = req.body.id;
         if (!reqdata.id) {
-            res.status(400).json('no ID in request body');
+            res.status(400).json('No ID in request body');
         } else {
             await deleteUser(data);
         }
@@ -54,7 +52,7 @@ export const getById = async (req, res) => {
     try {
         const reqdata = await req.body;
         if (!reqdata.id) {
-            res.status(400).json('no ID in request body');
+            res.status(400).json('No ID in request body');
         } else {
             const dbdata = await findbyIdUser(reqdata);
             res.status(200).json(dbdata);
@@ -68,7 +66,7 @@ export const update = async (req, res) => {
     try {
         const reqdata = await req.body;
         if (!reqdata.id) {
-            res.status(400).json('no ID in request body');
+            res.status(400).json('No ID in request body');
         } else {
             const dbdata = await updateUser(reqdata);
             res.status(202).json(dbdata);
