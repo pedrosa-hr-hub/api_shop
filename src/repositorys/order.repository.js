eaf59484dd0374be4ee0b5a5fc9a376a0f1d24e8
Pedro de Sá <pedro.trabalhos@outlook.com>
2023-04-18 22:32:29 -0300

    order basic querys on database and ajust in database

---
+++ src/repositorys/order.repository.js
@@ -0,0 +1,44 @@
+import { prisma } from '../services/prisma';
+
+export const createOrder = async (data) => {
+    const order = await prisma.order.create({
+        data,
+    });
+    return order;
+};
+
+export const updateOrder = async (data) => {
+    const order = await prisma.order.update({
+        where: {
+            id: data.id,
+        },
+        data: {
+            idUser: data.idUser,
+            idProduct: data.idProduct,
+        },
+    });
+    return order;
+};
+
+export const deleteOrder = async (data) => {
+    const order = await prisma.order.delete({
+        where: {
+            id: data.id,
+        },
+    });
+    return order;
+};
+
+export const findAllOrder = async () => {
+    const order = await prisma.order.findMany();
+    return order;
+};
+
+export const findbyIdOrder = async (data) => {
+    const order = await prisma.order.findUnique({
+        where: {
+            id: data.id,
+        },
+    });
+    return order;
+};
